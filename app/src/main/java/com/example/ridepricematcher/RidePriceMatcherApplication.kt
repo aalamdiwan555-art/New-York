5f4b81b2e3d9ef1cb83e3dc5ed21077691c24290
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-27 23:27:43 +0530

    fix: add Autopilot launcher icon

--- app/src/main/java/com/example/ridepricematcher/RidePriceMatcherApplication.kt
+++ app/src/main/java/com/example/ridepricematcher/RidePriceMatcherApplication.kt
@@ -1,51 +1,12 @@
 package com.example.ridepricematcher
 
 import android.app.Application
-import androidx.room.Room
 import com.example.ridepricematcher.ads.UnityAdsManager
-import com.example.ridepricematcher.data.local.AppDatabase
-import com.example.ridepricematcher.data.repository.*
 
 class RidePriceMatcherApplication : Application() {
-
-    lateinit var database: AppDatabase
-        private set
-
-    lateinit var authRepository: AuthRepository
-        private set
-
-    lateinit var entitlementRepository: EntitlementRepository
-        private set
-
-    lateinit var languageRepository: LanguageRepository
-        private set
-
-    lateinit var userPreferenceRepository: UserPreferenceRepository
-        private set
-
-    lateinit var adminRepository: AdminRepository
-        private set
-
     override fun onCreate() {
         super.onCreate()
-        instance = this
-
-        database = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "autopilot.db"
-        ).build()
-
-        authRepository = AuthRepository()
-        entitlementRepository = EntitlementRepository(database.entitlementDao())
-        languageRepository = LanguageRepository(database.languageDao(), database.phraseDao())
-        userPreferenceRepository = UserPreferenceRepository(database.userPreferenceDao())
-        adminRepository = AdminRepository()
+        AppModule.init(this)
         UnityAdsManager.initialize(this)
-    }
-
-    companion object {
-        lateinit var instance: RidePriceMatcherApplication
-            private set
     }
 }
